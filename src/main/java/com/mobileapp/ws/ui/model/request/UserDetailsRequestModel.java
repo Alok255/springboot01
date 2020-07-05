3c791b18e2713da7407e64403d6f79d3f7ae1a7f
alok255 <alokrajsingh255@gmail.com>
2020-07-05 01:03:40 -0400

    added mysql connector, spring data jpa and request response model

---
+++ src/main/java/com/mobileapp/ws/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,38 @@
+package com.mobileapp.ws.ui.model.request;
+
+import lombok.*;
+
+/*@Getter
+@Setter*/
+public class UserDetailsRequestModel {
+
+	private String firstname;
+	private String lastName;
+	private String email;
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String password;
+}
